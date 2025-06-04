a6624f03ec9718102f9208e34c1b28b4c7766688
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-06-04 11:10:29 +0100

    Delete database directory

--- database/antibot.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const antiBotSchema = new mongoose.Schema({
-  groupId: { type: String, required: true, unique: true },
-  enabled: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('AntiBot', antiBotSchema);
--- database/chatbot.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const chatbotSchema = new mongoose.Schema({
-    groupId: { type: String, required: true, unique: true },
-    enabled: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('Chatbot', chatbotSchema);
